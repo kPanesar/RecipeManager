fdebbdbb629bad4bf0b0d516a8d126c1b95adf0d
Karanvir Panesar <singh.karanvir21@gmail.com>
2016-11-08 01:38:12 -0800

    Added Image to show page
    Added functionality to display the recipe image on the show page.

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -3,9 +3,18 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-sm-3">
+                @if($recipe->photo != '')
+                    <img src="{{ asset('uploads/'.$recipe->photo) }}" class="img-responsive" alt="{{ $recipe->name }}">
+                @endif
+            </div>
+        </div>
+        <div class="row">
             <h1 class="edit_text">{{$recipe->name}}</h1>
             <p>{{$recipe->description}}</p>
             <a href="{{ route('recipes.edit', $recipe->id) }}" class="btn btn-default">Edit</a>
+
+            {{--Delete Button--}}
             {!! Form::open(array(
                               'style' => 'display: inline-block;',
                               'method' => 'DELETE',
@@ -13,6 +22,7 @@
                               'route' => ['recipes.destroy', $recipe->id])) !!}
             {!! Form::submit('Delete', array('class' => 'btn btn-primary')) !!}
             {!! Form::close() !!}
+
         </div>
     </div>
 @stop
