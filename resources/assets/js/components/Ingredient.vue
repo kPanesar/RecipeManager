05ffece1f3cb827ba082ae4a0a1cefdeed01a035
Karanvir Panesar <singh.karanvir21@gmail.com>
2016-11-11 01:18:57 -0800

    Added form styling to ingredient

--- resources/assets/js/components/Ingredient.vue
+++ resources/assets/js/components/Ingredient.vue
@@ -1,31 +1,32 @@
 <template>
     <div class="row">
-        <div class="col-sm-1">
+        <div class="col-sm-2">
             <div class="form-group">
                 <label for="quantity">
-                <input ref="input" id="quantity" name="quantity" type="text"
+                <input ref="input" id="quantity" class="form-control" name="quantity" type="number"
                        v-bind:value="quantity"
                        v-on:input="checkQuantity($event.target.value)">
             </div>
         </div>
 
-        <div class="col-sm-1">
+        <div class="col-sm-2">
             <div class="form-group">
                 <label for="unit">
-                <input id="unit" name="unit" type="text"
+                <input id="unit" name="unit" class="form-control" type="text"
                        v-bind:value="unit"
                        v-on:input="checkUnit($event.target.value)">
             </div>
         </div>
 
-        <div class="col-sm-1">
+        <div class="col-sm-4">
             <div class="form-group">
                 <label for="ingredient_name">
-                <input id="ingredient_name" name="ingredient_name" type="text"
+                <input id="ingredient_name" class="form-control" name="ingredient_name" type="text"
                        v-bind:value="name"
                        v-on:input="checkName($event.target.value)">
             </div>
         </div>
+
     </div>
 </template>
 
