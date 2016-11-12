e260c8e26c08bdb0b95ce82e0a6675ef200ec50c
Karanvir Panesar <singh.karanvir21@gmail.com>
2016-11-12 04:46:53 -0800

    Started work on adding ingredients

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -2,22 +2,42 @@
 
 @section('content')
     <div class="container">
+        {!! Form::open(['url' => 'recipes']) !!}
         <div class="row">
-            <h1>Create a Recipe</h1>
-            {!! Form::open(['url' => 'recipes']) !!}
+            <div class="colxs-12">
+                <h1>Create a Recipe</h1>
                 <div class="form-group">
                     {!! Form::label('name', 'Recipe Name') !!}
                     {!! Form::text('name', null, ['class' => 'form-control']) !!}
                 </div>
-            <div class="form-group">
-                {!! Form::label('description', 'Description') !!}
-                {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+                <div class="form-group">
+                    {!! Form::label('description', 'Description') !!}
+                    {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+                </div>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 input-group">
+                <span>
+                    <h4>Ingredients <button class="btn btn-default pull-right">
+                        Add
+                    </button></h4>
+                </span>
+                <div class="input-group">
+                    <input type="text" name="ingredient[]" class="form-control" />
+                    <span class="input-group-addon"> </span>
+                    <input type="text" name="ingredient[]" class="form-control" />
+                    <span class="input-group-addon"> </span>
+                    <input type="text" name="ingredient[]" class="form-control" />
+                </div>
+            </div>
+        </div>
+        <div class="row">
             <div class="form-group">
                 {!! Form::submit('Create Recipe', ['class' => 'btn btn-primary form-control']) !!}
             </div>
-            {!! Form::close() !!}
         </div>
+        {!! Form::close() !!}
     </div>
 @stop
 
